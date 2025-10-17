f48a08e703254504db89b774fd49069fb6ae31cf
binkowski <binkowskimateusz97@gmail.com>
2025-10-17 10:37:39 +0200

    Delete delay on select menu hide

--- src/app/button-menu-bar/button-menu-bar.ts
+++ src/app/button-menu-bar/button-menu-bar.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectMenu } from './select-menu/select-menu';
 
@@ -9,38 +9,14 @@ import { SelectMenu } from './select-menu/select-menu';
   templateUrl: './button-menu-bar.html',
   styleUrl: './button-menu-bar.scss'
 })
-export class ButtonMenuBar implements OnDestroy {
+export class ButtonMenuBar {
   hoveredButton: string | null = null;
-  private hideTimeout: any = null;
 
   onButtonHover(buttonName: string) {
-    // Clear any pending hide timeout
-    if (this.hideTimeout) {
-      clearTimeout(this.hideTimeout);
-      this.hideTimeout = null;
-    }
     this.hoveredButton = buttonName;
   }
 
   onContainerLeave() {
-    // Add a small delay to prevent flickering when moving between elements
-    this.hideTimeout = setTimeout(() => {
-      this.hoveredButton = null;
-    }, 200);
-  }
-
-  onMenuEnter() {
-    // Cancel hide if cursor enters the menu
-    if (this.hideTimeout) {
-      clearTimeout(this.hideTimeout);
-      this.hideTimeout = null;
-    }
-  }
-
-  ngOnDestroy() {
-    // Clean up timeout on component destroy
-    if (this.hideTimeout) {
-      clearTimeout(this.hideTimeout);
-    }
+    this.hoveredButton = null;
   }
 }
